5f142d7d6fa62ea94fb477154a7394b0f624c6a0
Mohamed Ganoub <mhmdganoub@gmail.com>
2026-09-19 16:34:47 +0200

    start adding jwt bearer

--- src/Notch.Api/Program.cs
+++ src/Notch.Api/Program.cs
@@ -1,5 +1,9 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Notch.Api.Data;
+using Notch.Api.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,9 +12,37 @@ builder.Services.AddDbContext<NotchDbContext>(options =>
 {
     options.UseNpgsql(builder.Configuration.GetConnectionString("Default"));
 });
+
+builder.Services.AddIdentityCore<AppUser>(options =>
+{
+    options.Password.RequiredLength = 8;
+    options.User.RequireUniqueEmail = false;
+})
+.AddEntityFrameworkStores<NotchDbContext>();
+
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!))
+        };
+    });
+builder.Services.AddAuthentication();
 
 var app = builder.Build();
 
@@ -21,6 +53,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
---
+++ src/Notch.shared/Dto/AuthDtos.cs
@@ -0,0 +1,8 @@
+﻿namespace Notch.Shared.Dto;
+
+public record RegisterRequest(string UserName, string Password);
+public record LoginRequest(string UserName, string Password);
+
+public record TokenReponse(string AccessToken, string RefreshToken, DateTime AccessTokenExpiresAtUtc);
+
+public record RefreshRequest(string RefreshToken);
