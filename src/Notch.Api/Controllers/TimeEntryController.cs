0aa383ec8faaa0b7377e9e228757a875d2488cb7
Mohamed Ganoub <mhmdganoub@gmail.com>
2026-09-23 20:15:42 +0200

    add time entries api

---
+++ src/Notch.Api/Controllers/TimeEntryController.cs
@@ -0,0 +1,86 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Notch.Api.Data;
+using Notch.Api.Models;
+using Notch.Shared.Dto;
+
+namespace Notch.Api.Controllers;
+
+[ApiController]
+[Route("api/timeentries")]
+[Authorize]
+public class TimeEntryController : ControllerBase
+{
+    private readonly NotchDbContext _db;
+
+    public TimeEntryController(NotchDbContext db)
+    {
+        _db = db;
+    }
+
+    [HttpPost("start")]
+    public async Task<ActionResult<TimeEntryDto>> StartTask(StartTimerRequest request)
+    {
+        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        var task = await _db.TaskItems
+            .AnyAsync(t => t.Id == request.TaskItemId && t.UserId == userId);
+        if (!task)
+        {
+            return NotFound("Task not found");
+        }
+        
+        // auto-stop whatever's currently running
+        var currentTimeEntry = await _db.TimeEntries
+            .FirstOrDefaultAsync(e => e.EndedAt == null && e.UserId == userId);
+        if (currentTimeEntry is not null)
+        {
+            currentTimeEntry.EndedAt = DateTime.UtcNow;
+            await _db.SaveChangesAsync();
+        }
+        var timeEntry = new TimeEntry
+        {
+            TaskItemId = request.TaskItemId,
+            UserId = userId!,
+        };
+        await _db.TimeEntries.AddAsync(timeEntry);
+        await _db.SaveChangesAsync();
+        var dto = new TimeEntryDto(timeEntry.Id, timeEntry.TaskItemId, timeEntry.StartedAt, timeEntry.EndedAt);
+        return Ok(dto);
+    }
+
+    [HttpPost("stop")]
+    public async Task<ActionResult<TimeEntryDto>> StopTask()
+    {
+        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        var currentTimeEntry = await _db.TimeEntries
+            .FirstOrDefaultAsync(t => t.EndedAt == null && t.UserId == userId);
+        if (currentTimeEntry is null)
+        {
+            return NoContent();
+        }
+
+        currentTimeEntry.EndedAt = DateTime.UtcNow;
+        await _db.SaveChangesAsync();
+        var currentTimeDto = new TimeEntryDto(currentTimeEntry.Id,  currentTimeEntry.TaskItemId,  currentTimeEntry.StartedAt, currentTimeEntry.EndedAt);
+        return Ok(currentTimeDto);
+    }
+    
+    [HttpGet("current")]
+    public async Task<ActionResult<TimeEntryDto?>> GetCurrent()
+    {
+        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+        var currentTimeEntry = await _db.TimeEntries
+            .FirstOrDefaultAsync(t => t.EndedAt == null && t.UserId == userId);
+
+        if (currentTimeEntry is null)
+        {
+            return Ok();
+        }
+        var currentTimeDto = new TimeEntryDto(currentTimeEntry.Id,  currentTimeEntry.TaskItemId,  currentTimeEntry.StartedAt, currentTimeEntry.EndedAt);
+        return Ok(currentTimeDto);
+    }
+}
---
+++ src/Notch.shared/Dto/TimeEntryDtos.cs
@@ -0,0 +1,9 @@
+﻿namespace Notch.Shared.Dto;
+
+public record StartTimerRequest (Guid TaskItemId);
+
+public record TimeEntryDto
+(
+    Guid Id, Guid TaskItemId, DateTime StartedAtUtc,
+     DateTime? EndedAtUtc
+);
